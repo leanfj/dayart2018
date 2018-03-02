b246cac4127777e6524429cdcc8bd507d9ac3b0f
leanfj <leanfj@hotmail.com>
2018-03-02 19:26:52 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
@@ -22,6 +25,20 @@ gulp.task('sass', function () {
         .pipe(sass())
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
+});
+
+//Concatenar Javascript
+var jquery = 'node_modules/jquery/dist/jquery.js';
+var bootstrap = 'node_modules/bootstrap/dist/js/bootstrap.js';
+var mainJS = 'js/main.js'
+
+gulp.task('concatjs', function () {
+    return gulp.src([jquery, bootstrap, mainJS])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./js/'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/'));
 });
 
 gulp.task('default', ['serve']);
